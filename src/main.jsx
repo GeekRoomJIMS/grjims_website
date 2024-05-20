a9a0538b3a94d68f546e7e5bb6510f93a3d1fde6
Anshi-0412 <simmighosh738@gmail.com>
2024-05-20 13:13:18 +0530

    faqs done.

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Footer from './components/Footer.jsx';
+import Faq from './components/FAQ/Faq.jsx';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <Faq />
     <Footer />
   </React.StrictMode>,
 )
