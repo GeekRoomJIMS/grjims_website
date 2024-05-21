156373c0d12a8945be747b6d34f41c50bb65e456
Anshi-0412 <simmighosh738@gmail.com>
2024-05-21 11:34:55 +0530

    faqs.

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import Faq from "./components/FAQ/Faq.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
+    <Faq />
   </React.StrictMode>
 );
