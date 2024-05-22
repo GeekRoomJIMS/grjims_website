b6397c431d59940b21e9f6fd0e33594f4d728b9e
Anshi-0412 <simmighosh738@gmail.com>
2024-05-22 22:44:29 +0530

    footer.

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import Faq from "./components/FAQ/Faq.jsx";
+import Footer from "./components/Footer.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
     <Faq />
+    <Footer />
   </React.StrictMode>
 );
