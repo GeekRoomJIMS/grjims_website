a24313f81af69647176471a75ad89485b03851b7
sanchxt <tpbeast007@gmail.com>
2024-05-22 10:36:40 +0530

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ import "./assets/scss/main.scss";
 import Index from "./components/Index";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Contact from "./components/Contact"
+import Contact from "./components/Contact";
 function App() {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/" element={<Index />} exact />
+        <Route path="/" element={<Index />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
       <Footer />
