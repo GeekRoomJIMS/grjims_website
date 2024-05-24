0d75db5975a8d3a2bc1629d97404fc6702368ff9
sanchxt <tpbeast007@gmail.com>
2024-05-25 02:02:01 +0530

    update carousel

--- src/components/CarouselComponent.jsx
+++ src/components/CarouselComponent.jsx
@@ -1,41 +1,40 @@
 import Carousel from "react-bootstrap/Carousel";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import img1 from "../assets/carousel-images/img1.jpg";
+import img2 from "../assets/carousel-images/img2.jpg";
+import img3 from "../assets/carousel-images/img3.jpg";
+
 function CarouselComponent() {
   return (
     <Carousel fade className="mt-5 h-81">
       <Carousel.Item>
-        <img
-          className="d-block w-full"
-          src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(15).webp"
-          alt="Sunset Over the City"
-        />
+        <img className="d-block w-full" src={img1} alt="Geek Room Event" />
         <Carousel.Caption>
-          <h5>First slide label</h5>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          <h5>Introduction Event</h5>
+          <p>A Geek Room Introduction event hosted in JIMS, Rohini</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
           className="d-block w-full"
-          src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(22).webp"
-          alt="Canyon at Night"
+          src={img2}
+          alt="Geek Room Hackathon Event"
         />
         <Carousel.Caption>
-          <h5>Second slide label</h5>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-full"
-          src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(23).webp"
-          alt="Cliff Above a Stormy Sea"
-        />
-        <Carousel.Caption>
-          <h5>Third slide label</h5>
+          <h5>Hackathon</h5>
           <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            Students coding in a hackathon, which was hosted by Geek Room JIMS
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img className="d-block w-full" src={img3} alt="Hackathon Trophies" />
+        <Carousel.Caption>
+          <h5>Hackathon Awards</h5>
+          <p>
+            Awards given out to deserving students and judges during Geek Room
+            JIMS' events
           </p>
         </Carousel.Caption>
       </Carousel.Item>
