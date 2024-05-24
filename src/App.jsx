5911e1b82562a599267f8b927435f3061f046ef7
Anshi-0412 <simmighosh738@gmail.com>
2024-05-24 23:49:52 +0530

    Merge pull request #19 from sanchxt/main
    background & ui fixes

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import "./app.css";
 import Events from "./components/events/Events";
 import Achievements from "./components/achievements/Achievements";
 import Blogs from "./components/blogs/Blogs";
+//import Contact from "./components/Contact";
+
 
 function App() {
   return (
@@ -16,6 +18,7 @@ function App() {
         <Route path="/events" element={<Events />} />
         <Route path="/achievements" element={<Achievements />} />
         <Route path="/blogs" element={<Blogs />} />
+        {/* <Route path="/contacts" element={<Contact />} /> */}
       </Routes>
     </BrowserRouter>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import Faq from "./components/FAQ/Faq.jsx";
+import Footer from "./components/Footer.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
+    <Faq />
+    <Footer />
   </React.StrictMode>
 );
+//end
