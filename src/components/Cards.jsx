ef35c7ae8c46b18fe9820acd76b991d88cab9035
sam1132 <sam113273@gmail.com>
2024-06-05 16:25:27 +0530

    Events cards

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -4,7 +4,7 @@ import cardImage from "../assets/images/168.webp";
 const Cards = ({ date, title, description, teams, status }) => {
   return (
     <div className=" max-w-[20rem] md:max-w-7xl mx-auto my-3 md:my-10">
-      <div className="max-w-[23rem] mx-auto  bg-[#fffafa] rounded-lg shadow ">
+      <div className="max-w-[23rem] mx-auto  bg-black rounded-lg shadow  ">
         <a href="#">
           <img
             className="rounded-t-lg"
@@ -14,23 +14,23 @@ const Cards = ({ date, title, description, teams, status }) => {
         </a>
         <div className="p-3 md:p-4">
           <a href="#">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-slate-900 ">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-[#fffafa] ">
               {title}
             </h5>
           </a>
-          <p className="mb-3 font-normal text-gray-800 ">{description}</p>
+          <p className="mb-3 font-normal text-[#ffffff98] ">{description}</p>
 
           <div className="mb-4">
             {teams ? (
-              <h2 className="text-slate-900 md:text-xl text-base font-bold ">
+              <h2 className="text-[#fffafa] md:text-xl text-base font-bold ">
                 Teams Participated {teams}
               </h2>
             ) : (
               ""
             )}
-            <p className="text-slate-900 font-bold text-lg">{date}</p>
+            <p className="text-[#fffafa] font-bold text-lg">{date}</p>
           </div>
-          <button className="inline-flex items-center px-3 py-2 text-sm duration-700 text-center font-bold hover:text-slate-900 hover:bg-[#80ed99] rounded-lg bg-slate-900 text-[#80ed99] ">
+          <button className="inline-flex items-center px-3 py-2 text-sm duration-700 text-center font-bold text-slate-900 bg-[#80ed99] rounded-lg ">
           {status}
           </button>
         </div>
