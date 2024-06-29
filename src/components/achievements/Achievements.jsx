628fe701eb42048fbc98e3c5f13f726139345dc6
sanchxt <tpbeast007@gmail.com>
2024-06-29 15:59:05 +0530

    add achievements page

--- src/components/achievements/Achievements.jsx
+++ src/components/achievements/Achievements.jsx
@@ -1,5 +1,75 @@
-const Achievements = () => {
-  return <div className="flex justify-center text-6xl">Coming Soon</div>;
+import React from "react";
+import styles from "./index.module.css";
+import Navbar from "../hero/Navbar";
+import Footer from "../Footer";
+
+const Index = () => {
+  const cardData = [
+    {
+      front: { title: "Code Kshetra", para: "Hackathon" },
+      back: {
+        title: "Hackathon",
+        para: "Hosted Delhi's biggest hackathon, hosted @ JIMS, Rohini",
+      },
+    },
+    {
+      front: { title: "Techfest", para: "Hackathon" },
+      back: {
+        title: "Hackathon",
+        para: "Hosted a hackathon @ IITM, Janakpuri",
+      },
+    },
+    {
+      front: { title: "Code Cubicle", para: "Hackathon" },
+      back: {
+        title: "Hackathon",
+        para: "Hosted a hackathon @ Eccosphere Coworking Space, Noida",
+      },
+    },
+    {
+      front: { title: "Algohunt", para: "Problem-Solving Event" },
+      back: {
+        title: "Event",
+        para: "Hosted a treasure-styled problem solving event",
+      },
+    },
+  ];
+
+  return (
+    <>
+      <Navbar />
+      <div className={styles.container}>
+        <div className="maincontainer">
+          <div className={styles.innercontainer}>
+            <div className={styles.bgimg}>
+              <img src="/achievements_page/1.png" alt="" />
+            </div>
+            <div className={styles.img}>
+              <img src="/achievements_page/1.png" alt="" />
+            </div>
+            <div className={styles.content}>
+              <div className={styles.heading}>achievements</div>
+              <div className={styles.cardscontainer}>
+                {cardData.map((card, index) => (
+                  <div key={index} className={styles.cardcontainer}>
+                    <div className={`${styles.card} ${styles.cardfront}`}>
+                      <h3>{card.front.title}</h3>
+                      <p>{card.front.para}</p>
+                    </div>
+                    <div className={`${styles.card} ${styles.cardback}`}>
+                      <h3>{card.back.title}</h3>
+                      <p>{card.back.para}</p>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
 };
 
-export default Achievements;
+export default Index;
