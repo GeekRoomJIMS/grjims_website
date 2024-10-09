d38c22ba6ef69380346c8c643eae0dc4189a3579
sanchxt <tpbeast007@gmail.com>
2024-10-09 20:48:54 +0530

    update navbar

--- src/components/hero/Navbar.jsx
+++ src/components/hero/Navbar.jsx
@@ -3,7 +3,7 @@ import Nav from "./Nav";
 
 const Navbar = () => {
   return (
-    <header className="bg-dark-background top-0 flex-wrap z-[20] mx-auto flex w-full items-center justify-between border-gray-500 p-8">
+    <header className="bg-dark-background top-0 flex-wrap z-[20] mx-auto flex w-full items-center justify-between border-gray-500 px-6 py-2 border-b border-white/10 backdrop-blur-3xl fixed shadow-xl shadow-blue-300/5">
       <Logo />
       <Nav />
     </header>
